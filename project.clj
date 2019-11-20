e050891867d23a3910f139178e3a3f200d80b0d8
Chinnon Santos <chinnonsantos@gmail.com>
2019-11-20 09:57:17 +0000

    Fixing embedded nREPL

--- project.clj
+++ project.clj
@@ -10,15 +10,16 @@
                  ]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler account-service.service/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]
-                        [midje "1.9.9"] ;; TDD
-                        [ring/ring-core "1.7.1"]
-                        [ring/ring-jetty-adapter "1.7.1"] ;; Ring server abstraction
-                        ]
-         :plugins [[lein-midje "3.2.1"]
-                   [lein-cloverage "1.1.2"] ;; Test coverage
-                   ]}}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]
+                                  [midje "1.9.9"] ;; TDD
+                                  [ring/ring-core "1.7.1"]
+                                  [ring/ring-jetty-adapter "1.7.1"] ;; Ring server abstraction
+                                  [cljfmt "0.6.5"] ;; Code formatting
+                                  ]
+                   :plugins [[lein-midje "3.2.1"]
+                             [lein-cloverage "1.1.2"] ;; Test coverage
+                             [cider/cider-nrepl "0.22.4"] ;; embedded nREPL
+                             ]}}
   :test-paths ["test/unit"
                "test/assertion"])
